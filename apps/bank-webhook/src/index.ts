cde50f5a6c95cd51d00e7e4044e80358f901dfcc
Abdul Mannan <mannan.jnv123@gmail.com>
2025-01-22 19:48:35 +0530

    webhook

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import { PrismaClient } from "@abdul1mannan/db/client";
+import { z } from "zod";
+
+const db = new PrismaClient();
+const app = express();
+
+app.use(express.json());
+
+app.post("/hdfcWebhook", async (req, res) => {
+  const { token, userId, amount } = req.body;
+  const paymentInformation = {
+    token,
+    userId: userId,
+    amount: Number(amount),
+  };
+
+  try {
+    await db.$transaction([
+      db.balance.updateMany({
+        where: { userId: paymentInformation.userId },
+        data: {
+          amount: { increment: Number(paymentInformation.amount) },
+        },
+      }),
+      db.onRampTransaction.updateMany({
+        where: { token: paymentInformation.token },
+        data: { status: "Completed" },
+      }),
+    ]);
+
+    res.json({ message: "Captured" });
+  } catch (e) {
+    console.error("Error processing webhook:", e);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.listen(3003, () => {
+  console.log(`Server running on port 3003`);
+});
